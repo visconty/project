94b305944ceb42f1e8b197ed65c8d99e85ba3588
GuruFront <ruslan.gunder@gmail.com>
2018-02-07 21:33:36 +0200

    fgh

---
+++ JS/Script.js
@@ -0,0 +1,101 @@
+// var test = document.getElementById('test');
+// test.innerHTML = 'Hello World!';
+
+// var balls = +prompt('Balls?');
+
+// if (balls > 94 && balls <= 100) {
+// 	alert('A');
+// } else if (balls > 84) {
+// 	alert ('B');
+// } else if (balls > 74) {
+// 	alert ('C');
+// } else if (balls > 64) {
+// 	alert ('D');
+// } else if (balls > 59) {
+// 	alert ('E');
+// } else if (balls >= 0) {
+// 	alert ('FX');
+// } else {
+// 	alert ('error');
+// }
+
+// var text;
+// var fruit = 'Banana';
+// switch (fruit) {
+// 	case 'Banana': text = 'Something!';
+// 	break;
+// 	case 'Orange': text = 'Nothing!';
+// 	break;
+// 	default: text = 'Not fruit';
+// }
+// alert(text);
+
+// for (var i = 0; i < 10; i++) {
+// 	console.log(i);
+// }
+
+// var j = 0;
+// while (j > 10) {
+// 	console.log(j);
+// 	if (j > 10) break;
+// 	j++;
+// }
+
+// var i = 0;
+// do {
+// 	i++;
+// 	console.log(111);
+// } while (i < 10)
+
+// var a = [];
+// var i;
+// for (var i = 0; i < 5; i++) {
+// 	a[i] = prompt('Num?');
+// 	if (a < 60) break;
+// 	console.log(a[i]);
+// 	}
+
+// while (true) {
+
+// 	var first = prompt('First?');
+// 	if (first === 'all') break;
+// 	var second = prompt('Second?');
+// 	if (first === 'all') break;
+// 	var operator = prompt('Operator?');
+// 	if (first === 'all') break;
+	
+
+// 	switch(operator) {
+// 		case '+':
+// 		alert(+first+ +second);
+// 		break;
+// 		case '-':
+// 		alert(+first- +second);
+// 		break;
+// 		case '*':
+// 		alert(+first* +second);
+// 		break;
+// 		case '/':
+// 		alert(+first/ +second);
+// 		break;
+// 		alert('Not operator');
+// 		break;
+// 	} 
+// }
+
+// var cars = [];
+
+// while (cars.length < 5) {
+// 	var firm = prompt('firm?');
+// 	var model = prompt('model?');
+// 	var ed = parseFloat(prompt('ed?'));
+
+// 	cars.push({firm: firm, model: model, ed: ed})
+// }
+
+// cars.filter(function(item) {
+// 	return item.ed > 2;
+// });
+
+// console.log(filtered);
+
